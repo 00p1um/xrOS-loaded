b8b3fac1101cee3096c8b6867e07dcf65575fab3
Rayan Khan <iRayanKhan@icloud.com>
2023-06-21 16:13:34 -0500

    Initial Commit

---
+++ xrOS-loaded/xrOS_loadedApp.swift
@@ -0,0 +1,17 @@
+//
+//  xrOS_loadedApp.swift
+//  xrOS-loaded
+//
+//  Created by Rayan Khan on 6/21/23.
+//
+
+import SwiftUI
+
+@main
+struct xrOS_loadedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
